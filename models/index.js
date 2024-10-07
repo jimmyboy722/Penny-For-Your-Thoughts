e935d15e836500ac535538a27080089171c923e7
jimmyboy722 <jeckman722@gmail.com>
2024-10-06 22:31:56 -0400

    coding and notation index.js of models

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// THIS FILE SERVES AS THE CENTRAL EXPORT FOR ALL MODELS
+// INSTEAD OF IMPORTING EACH MODEL SEPARATELY, THIS FILE IMPORTS ALL MODELS AT ONCE
+
+// IMPORTING USER AND THOUGHT MODELS
+const User = require("./User");
+const Thought = require("./Thought");
+
+module.exports = { User, Thought };
